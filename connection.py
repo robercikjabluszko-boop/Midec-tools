e67af664fad073836fef7fd68314acf3c23c6769
robercikjabluszko-boop <robercikjabluszko@gmail.com>
2026-07-29 07:43:53 +0200

    added detailed code documentation and comments to main

--- connection.py
+++ connection.py
@@ -1,26 +1,23 @@
-from tkinter import Menu, messagebox
-
-LOC = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
+from tkinter import Menu, messagebox # importing the framework to return the Menu(vistas) when legally gotten
 
 class menubar():
     def ver(self, window_context):
-        import requests
-        import json
-        url = "https://ubiquitous-figolla-2037da.netlify.app/variable.json"
+        import requests # importing requests for getting the legal licenses from the server
+        import json # importing json to get the license
+        url = "https://ubiquitous-figolla-2037da.netlify.app/variable.json" # the server, you need to replace it with your own
 
-        response = requests.get(url, timeout=5)
+        response = requests.get(url, timeout=5) # getting the response from the server
 
-        license_server = response.json()
+        license_server = response.json() # getting the json out of the server
 
-        with open("license.json", "r") as license:
-            license_read =  json.load(license)
+        with open("license.json", "r") as license: # opening the entered license
+            license_read =  json.load(license) # getting the users license
 
-        if license_read['user'] not in license_server['license']:
-            messagebox.showerror("You are in some trouble", "Get out, dont even try to battle it.")
-            window_context.destroy()
-            exit
-        else:
-            return Menu(window_context)
+        if license_read['user'] not in license_server['license']: # if user doesnt have a real license
+            messagebox.showerror("You are in some trouble", "Get out, dont even try to battle it.") # show them an error message
+            window_context.destroy() # destroy the vistas
+        else: # if user has a real license
+            return Menu(window_context) # return vistas so the app wont crash
 
-verification = menubar()
+verification = menubar() # tech stuff
 
--- main.py
+++ main.py
@@ -1,45 +1,46 @@
-from tkinter import Tk, Text, Menu, filedialog
-import about_us
-import connection
+from tkinter import Tk, Text, Menu, filedialog # importing modules needed for Window, Entry, Menubar and saving the files
+import about_us # this is the module that creates a window when you click about/about us
+import connection # importing the module behind the anti-piracy
 
-vistas = Tk()
-vistas.geometry("1400x780")
+vistas = Tk() # creating a windows, shortcut from Visible Task, although programmers mainly use root or tk = Tk()
+vistas.geometry("1400x780") # setting the size of the window when you open the app
 
-entry = Text(vistas, height=1000, width=1000)
-entry.pack()
+entry = Text(vistas, height=1000, width=1000) # Setting up the entry where user can write the file
+entry.pack() # If you have no idea what this is, youre not alone
 
-chosen = None
+chosen = None # This is making the chosen variable available for all functions, needed to: If you opened a file using openusersfile(), you can save the file
+              # clicking only once the save in menubar, without specifying the directory
 
 def openusersfile():
-    global chosen
-    chosen = filedialog.askopenfilename(title="Select a file")
-    if chosen:
-        entry.delete("1.0", "end")
-        with open(chosen, "r") as file:
-            entry.insert("1.0", file.read())
+    global chosen # This is making so you can use the chosen variable in this function
+    chosen = filedialog.askopenfilename(title="Select a file") # When user clicks the Open in the Menubar/file/open, it asks the user for the dir of the file
+    if chosen: # Preventing crash if user doesnt specify the dir
+        entry.delete("1.0", "end") # Cleaning out the users table, so then we can insert the file's content without having the last file's content on
+        with open(chosen, "r") as file: # Opening the dir file
+            entry.insert("1.0", file.read()) # Inserting the file's content into the users box
 
 def saveusersfile():
-    global chosen
-    if chosen:
-        with open(chosen, "w", encoding="utf-8") as file:
-            file.write(entry.get("1.0", "end-1c"))
-    else:
-        file_dir = filedialog.asksaveasfilename(filetypes=[('All', '*')], defaultextension=".txt")
-        if file_dir:
-            chosen = file_dir
-            chosen += '.txt'
-            with open(chosen, "w", encoding="utf-8") as file:
-                file.write(entry.get("1.0","end-1c"))
+    global chosen # This is making so you can use the chosen variable in this function
+    if chosen: # Preventing crash if user doesnt specify the dir
+        with open(chosen, "w", encoding="utf-8") as file: # Opening the dir file
+            file.write(entry.get("1.0", "end-1c")) # writing to the user-specified file
+    else: # If user doesnt specify the file
+        file_dir = filedialog.asksaveasfilename(filetypes=[('All', '*')], defaultextension=".txt") # Asking the user where and how to create a file
+        if file_dir: # Preventing crash if user doesnt specify the file
+            chosen = file_dir # synchronizing chosen with the file dir
+            chosen += '.txt' # making it txt file
+            with open(chosen, "w", encoding="utf-8") as file: # opening the file
+                file.write(entry.get("1.0","end-1c")) # writing to the file
 
 def abouwind():
-    about_us.main(vistas)
+    about_us.main(vistas) # The about us window opening function
 
-menubar = connection.verification.ver(vistas)
-filesect = Menu(menubar, tearoff=0)
+menubar = connection.verification.ver(vistas) # Here is the anti-piracy embedded script, if the license is wrong the app will crash
+filesect = Menu(menubar, tearoff=0) # Adding the file section into the menubar
 filesect.add_command(label="Open", command=openusersfile)
 filesect.add_command(label="Save", command=saveusersfile)
 menubar.add_cascade(label="File", menu=filesect)
-abousect = Menu(menubar, tearoff=0)
+abousect = Menu(menubar, tearoff=0) # Adding the about section in the menubar
 abousect.add_command(label="About us", command=abouwind)
 menubar.add_cascade(label="About", menu=abousect)
 
